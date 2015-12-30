57811ed381db6525db075202ab373f27c98b9281
Matteo Scurati <matteo.scurati@gmail.com>
2015-12-30 01:21:26 +0100

    Removed node modules

--- node_modules/grunt-bemo/docs/grunt/clean.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  options: { force: true },
-  tempDir: ['<%= package.paths.temp %>'],
-  buildDir: ['<%= package.paths.build %>']
-};
-
--- node_modules/grunt-bemo/docs/grunt/copy.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  replaceUrlFunctions: {
-    files: {
-      "<%= package.paths.temp %>/stylesheets/functions/_urls.sass": "<%= package.paths.templates %>/_url-functions.sass"
-    }
-  }
-}
-
